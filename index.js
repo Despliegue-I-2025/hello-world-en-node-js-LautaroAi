47eae6ac8f6efeb2625e5e14286e881f16ac1670
LautaroAI <119972411+LautaroAi@users.noreply.github.com>
2025-08-11 12:04:35 -0300

    add "Hola mundo"

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-
-  // Esta línea está comentada. Deberías descomentarla para que funcione
-  // y cambiar el texto que se envía como respuesta.
-
-  //res.end('Poné lo que quieras acá');
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.end("Hola mundo");
+  // gracias por la ayuda
 });
 
 server.listen(3000, () => {
-  console.log('Servidor escuchando en http://localhost:3000');
+  console.log("Servidor escuchando en http://localhost:3000");
 });
